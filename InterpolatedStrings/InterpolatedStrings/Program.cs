e03f52e4d495a2f8bf50c19530a92415511cca84
CARICOM12 <0190a@ukr.net>
2020-05-02 21:58:40 +0300

    Add files via upload

---
+++ InterpolatedStrings/InterpolatedStrings/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace InterpolatedStrings
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            // Both calls produce the same output that is similar to:
+            // Hello, Mark! Today is Wednesday, it's 19:40 now.
+
+            string name = "Robert";
+            var date = DateTime.Now;
+
+            Console.WriteLine("Hello, {0}! Today is {1}, it's {2:HH:mm} now.", name, date.DayOfYear, date);
+
+            Console.WriteLine($"Hello. {name}! Today is {date.DayOfWeek}, it's {date:HH:mm} now");
+        }
+    }
+}
